4de1f47b56fb35546af14168d911e12d7d1e8c05
milyukmihail <63051273+milyukmihail@users.noreply.github.com>
2020-09-29 11:53:07 +0300

    Add files via upload

--- filter.php
+++ filter.php
@@ -4,7 +4,7 @@ require_once 'InBd/Connection.php';
 
 $connection = new Connection();
 
-$price = $_POST['Price'];
+$price = $_POST['price'];
 
 $ot = $_POST['ot'];
 
@@ -14,6 +14,7 @@ $znak = $_POST['znak'];
 
 $quantity = $_POST['quantity'];
 
+
 $sql = 'SELECT * FROM products.product WHERE (in_stock_1+in_stock_2)<? AND ?<cost AND cost<?';
 
 $request = $connection->link->prepare($sql);
@@ -22,7 +23,9 @@ $request->execute();
 
 $resultSet = $request->get_result();
 
-//print_r($price);die;
+$text = $_POST["quantity"];
+
+echo 'Привет, ' . $text;
 
 print_r($resultSet->fetch_all());
 
